1d42f5833bbcd4108f49778693f213e725652fd9
Bohdan <bohdanimiela@gmail.com>
2019-10-24 16:43:56 +0200

    Fix connection

--- data/db.js
+++ data/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const db_URI = process.env.DB_URI;
+
 mongoose.connect(db_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
+
 mongoose.connection.on('connected', () => console.log(`Mongoose connected`));
 mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'));
 mongoose.connection.on('error', error => console.error(`Mongoose connection error: ${error}`));
