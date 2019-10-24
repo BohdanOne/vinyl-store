d0172e0b00ef8a9d369d0c28172b354ce41839da
Bohdan <bohdanimiela@gmail.com>
2019-10-24 16:47:39 +0200

    import mongoose

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
